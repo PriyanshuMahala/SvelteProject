5d07e282705aba0d7aebbf124f2b9e8deb3cde72
noobs <noobs@ASUS-TUF-F15>
2025-10-01 16:36:37 +0530

    Initial Commit

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+</nav>
+
+{@render children?.()}
+
+<p></p>
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let count = $state(0);
+
+    function increment(){
+        count++;
+    }
+</script>
+
+<h1>{count}</h1>
+
+
+<button class="text-blue-600 hover:text-blue-200 active:text-red-400 transition-all duration-200"
+onclick={increment} >Click here</button>
