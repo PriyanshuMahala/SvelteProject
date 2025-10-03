5ad15019958d31dbcc4eb4e4023c7e3f2cdba2e9
Priyanshu mahala <mahalapriyanshu35@gmail.com>
2025-10-03 06:18:53 +0000

    Modify +layout.svelte

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -10,8 +10,8 @@
 </svelte:head>
 
 <nav class="flex space-x-3 py-3" >
-	<a class="hover:text-gray-200 transition duration-200" href="/">Home</a>
-	<a class="hover:text-gray-200 transition duration-200" href="/about">About</a>
+	<a class="hover:text-blue-400 transition duration-200" href="/">Home</a>
+	<a class="hover:text-blue-400 transition duration-200" href="/about">About</a>
 </nav>
 
 {@render children?.()}
